eaff694a22b9bb436bffa6b7a6a23c5121d9b920
gfaruk02 <gfaruk02@gmail.com>
2023-08-21 20:33:02 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,57 @@
+//common function
+let total = 0;
+function getCalculateSelectedItem(target){
+    const selectedItemContainer = document.getElementById('item-count');
+    const itemName = target.childNodes[3].childNodes[3].innerText;
+    // console.log(itemName)
+    const li = document.createElement("li");
+    li.innerText = itemName;
+    selectedItemContainer.appendChild(li);
+    // console.log(selectedItemContainer)
+     //find price amount
+     const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
+    //  console.log(price)
+     total = parseFloat(total) + parseFloat(price);
+     const totalAmount = total.toFixed(2);
+     document.getElementById('total').innerHTML = totalAmount;
+     
+    //console.log(totalAmount)
+    //Coupon button enable 
+     const couponcontent = document.getElementById("coupon-button");
+     if( totalAmount >= 200){
+      //   couponcontent.disabled = false;
+      couponcontent.removeAttribute('disabled');
+      couponcontent.style.backgroundColor =" #E527B2"
+     }
+     //Make purchase button enable
+     const makePurchase = document.getElementById("make-purchase");
+     if( totalAmount > 0){
+      //   makePurchase.disabled = false; 
+      makePurchase.removeAttribute('disabled');
+      makePurchase.style.backgroundColor =" #E527B2"
+     }
+
+
+}
+
+function getcoupon(){
+    const couponCode = document.getElementById('coupon-field').value;
+    if(couponCode == "SELL200"){
+      // discount calculation and value set 
+       const discountAmount = total * .20;
+       const discountAmountTwoDecimal = discountAmount.toFixed(2);
+       document.getElementById('discount').innerText = discountAmountTwoDecimal;
+      //  Grand Total Calculation and Set 
+       const grandTotal = total - discountAmountTwoDecimal;
+       const grandTotalTwoDecimal = grandTotal.toFixed(2);
+       document.getElementById('grand-total').innerText = grandTotalTwoDecimal;
+    }
+    else{
+        alert('Invalid Coupon, try Use Promo Code: SELL200 ');
+    }
+}
+
+function setHomePage(){
+  location.reload();
+  // window.location.assign("index.html");
+}
